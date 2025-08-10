569c62ae831b4bd4d10a99c983f07d09bcdd219a
tokojimo <victor.mora@orange.fr>
2025-08-11 00:31:08 +0200

    build: use vite build with alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+import path from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
