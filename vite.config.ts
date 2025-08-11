f04d2988e99a09ee2601a70e01a97c94adb8088a
tokojimo <victor.mora@orange.fr>
2025-08-11 20:46:50 +0200

    chore: bypass tailwind wasm during tests

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,13 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  test: { environment: 'jsdom', globals: true },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    css: {
+      postcss: {
+        plugins: [],
+      },
+    },
+  },
 });
