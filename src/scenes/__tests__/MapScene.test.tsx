f8a14c6438fc1342476432a1ec0c770f86d8282e
tokojimo <victor.mora@orange.fr>
2025-08-11 21:39:26 +0200

    Use zone name in map toasts

--- src/scenes/__tests__/MapScene.test.tsx
+++ src/scenes/__tests__/MapScene.test.tsx
@@ -62,7 +62,7 @@ describe('MapScene', () => {
     await new Promise(r => setTimeout(r, 0));
     mapInstance.handlers.click({ lngLat: { lat: 45.7, lng: 5.9 } });
 
-    const toast = await screen.findByRole('button', { name: /Testville/ });
+    const toast = await screen.findByRole('button', { name: new RegExp(DEMO_ZONES[1].name) });
     fireEvent.click(toast);
 
     expect(onZone).toHaveBeenCalledWith(expect.objectContaining({ id: DEMO_ZONES[1].id }));
