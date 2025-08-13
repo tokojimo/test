1ea2658547b0eddd48e200453c4b8f7689ae92fb
tokojimo <victor.mora@orange.fr>
2025-08-13 18:38:33 +0200

    feat(ui): lower toast vertical offset

--- src/components/ui/Toaster.tsx
+++ src/components/ui/Toaster.tsx
@@ -22,9 +22,9 @@ type Position =
   | 'bottom-center';
 
 const POS: Record<Position, string> = {
-  'top-right': 'top-4 right-4 items-end',
-  'top-left': 'top-4 left-4 items-start',
-  'top-center': 'top-4 left-1/2 -translate-x-1/2 items-center',
+  'top-right': 'top-8 right-4 items-end',
+  'top-left': 'top-8 left-4 items-start',
+  'top-center': 'top-8 left-1/2 -translate-x-1/2 items-center',
   'bottom-right': 'bottom-4 right-4 items-end',
   'bottom-left': 'bottom-4 left-4 items-start',
   'bottom-center': 'bottom-4 left-1/2 -translate-x-1/2 items-center',
